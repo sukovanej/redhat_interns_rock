5cae8d34c5b864ef42c83a522c4419d591510d9d
sukovanej <Milansuk@email.cz>
2018-01-21 13:36:51 +0100

    app

---
+++ app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <div>
+      <div class="text-xs-center">
+        <v-btn color="primary" @click="alert = !alert">Toggle</v-btn>
+      </div>
+      <v-alert color="error" :value="alert">
+        Whoops! An error occured!
+      </v-alert>
+    </div> 
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      alert: false
+    }
+  }
+}
+</script>
